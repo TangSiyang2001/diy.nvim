8cc6a528e3811f66cd106b8e560517ac580faf2e
tsy <tangsiyang2001@foxmail.com>
2024-12-02 14:08:49 +0800

    enable pyright and rust analyzer

--- lua/plugins/extensions/lsp-servers.lua
+++ lua/plugins/extensions/lsp-servers.lua
@@ -66,8 +66,8 @@ return {
     end,
   },
   -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
+  pyright = {},
+  rust_analyzer = {},
   -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
   --
   -- Some languages (like typescript) have entire language plugins that can be useful:
