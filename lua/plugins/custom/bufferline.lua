7ca0cc6d624c6a314360d96762b670ad807a38af
HastyShell <tangsiyang2001@foxmail.com>
2025-05-19 02:55:53 +0800

    1. add close current buffer key map 2. delete rarely used buffer key maps

--- lua/plugins/custom/bufferline.lua
+++ lua/plugins/custom/bufferline.lua
@@ -6,12 +6,11 @@ return {
     { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Delete Non-Pinned Buffers' },
     { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
     { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
+    { '<leader>bd', '<Cmd>bdelete!<CR>', desc = 'Close the current buffer' },
     { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
     { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
-    { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
-    { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
-    { '[B', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer prev' },
-    { ']B', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
+    { '[b', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer prev' },
+    { ']b', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
   },
   opts = {
     options = {
