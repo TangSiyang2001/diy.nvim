d3dc04e0490a0b132102275bdcb8636905a84d02
Siyang Tang <tangsiyang2001@foxmail.com>
2025-11-10 23:27:41 +0800

    misc fix

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,13 +4,12 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
-  lazy = false,
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
+  lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return { -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
-  -- event = 'VimEnter',
-  branch = '0.1.x',
+  event = 'VimEnter',
+  -- see https://github.com/nvim-lua/kickstart.nvim/pull/1448
+  -- branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
